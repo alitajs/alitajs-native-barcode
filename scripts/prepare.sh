78d9c62a1003fa3f3c175fa99797783b02f9f8f6
Ashoka <savebtc11@gmail.com>
2022-02-09 18:11:20 +0800

    fix: fix nvm install

---
+++ scripts/prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source ~/.bashrc
+nvm install lts/gallium
+nvm use lts/gallium
+npm install
